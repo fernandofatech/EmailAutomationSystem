4ed7852e8508f4c6c5ab52451622bd46c2fa5ac0
Fernando Francisco Azevedo <39699305+fernandofatech@users.noreply.github.com>
2022-02-12 22:54:27 -0300

    deploy
    Deploy

---
+++ secret.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+gmail_address = "insert.email.here@gmail.com"  # Email address sending out the reminder emails
+gmail_name = "AutoRemind System"  # E.g. Name of your lab
+gmail_password = "insertpasswordhere"
+admin_address = "insert.email.here@gmail.com"
